7f23f3e00c67674b8f866bd409eccfdf4fffd693
White1943 <whiteyang1943@gmail.com>
2024-07-04 18:01:14 +0800

    补充swagger,部分控制器的swagger说明

--- PetHomePlatform/src/main/java/com/example/platform/controller/PetController.java
+++ PetHomePlatform/src/main/java/com/example/platform/controller/PetController.java
@@ -8,6 +8,9 @@ import com.example.platform.service.AdoptionService;
 import com.example.platform.service.PetService;
 import com.example.platform.uilt.Base64Util;
 import com.example.platform.uilt.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,74 +22,90 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
+
 @Log4j
 @RestController
 @RequestMapping("/pet/")
+@Api(tags = "宠物管理接口", description = "提供宠物管理相关的 API")
 public class PetController {
 
     @Autowired
     private PetService petService;
 
-    @Value("${dir}")
-    private  String imgPath;
-
-
-
-
+    @Value("${dir}")//优雅,dir的配置见application.yml
+    private String imgPath;
 
     @PostMapping("petFindById")
-    public Result systemPetFindById(@RequestBody Pet pet){
+    @ApiOperation("根据ID查询宠物")
+    @ApiImplicitParam(name = "pet", value = "宠物实体", required = true, dataType = "Pet")
+    public Result systemPetFindById(@RequestBody Pet pet) {
         return petService.petFindById(pet);
     }
 
     @PostMapping("petListByUser")
-    public Result systemPetUserList(@RequestBody SearchInfo searchInfo){
+    @ApiOperation("根据用户查询宠物列表")
+    @ApiImplicitParam(name = "searchInfo", value = "查询条件", required = true, dataType = "SearchInfo")
+    public Result systemPetUserList(@RequestBody SearchInfo searchInfo) {
         return petService.petUserAll(searchInfo);
     }
 
     @PostMapping("petListAndPage")
-    public Result systemPetList(@RequestBody PageInfo page){
+    @ApiOperation("分页查询宠物列表")
+    @ApiImplicitParam(name = "page", value = "分页信息,msg='pet_name',可以晒名查询", required = true, dataType = "PageInfo")
+    public Result systemPetList(@RequestBody PageInfo page) {
         return petService.petListByUser(page);
     }
 
     @PostMapping("petInsert")
-    public Result systemPetInsert(@RequestBody Pet pet){
+    @ApiOperation("插入宠物")
+    @ApiImplicitParam(name = "pet", value = "宠物实体", required = true, dataType = "Pet")
+    public Result systemPetInsert(@RequestBody Pet pet) {
         return petService.petInsert(pet);
     }
 
     @DeleteMapping("petRemove")
-    public Result systemPetRemove(@RequestBody Pet pet){
+    @ApiOperation("删除宠物")
+    @ApiImplicitParam(name = "pet", value = "宠物实体仅id即可", required = true, dataType = "Pet")
+    public Result systemPetRemove(@RequestBody Pet pet) {
         return petService.petRemove(pet);
     }
 
     @PostMapping("petUpdate")
-    public Result systemPetUpdate(@RequestBody Pet pet){
+    @ApiOperation("更新宠物信息")
+    @ApiImplicitParam(name = "pet", value = "宠物实体", required = true, dataType = "Pet")
+    public Result systemPetUpdate(@RequestBody Pet pet) {
         return petService.petUpdate(pet);
     }
 
     @PostMapping("petStatusUpdate")
-    public Result systemPetStatusUpdate(@RequestBody Pet pet){
+    @ApiOperation("更新宠物状态")
+    @ApiImplicitParam(name = "pet", value = "宠物实体,含isadopted信息", required = true, dataType = "Pet")
+    public Result systemPetStatusUpdate(@RequestBody Pet pet) {
         return petService.petStatusUpdate(pet);
     }
 
     @PostMapping("petImgSelect")
-    public Result systemPetpetImgSelect(@RequestBody Pet pet){
+    @ApiOperation("查询宠物图片")
+    @ApiImplicitParam(name = "pet", value = "宠物实体,需包含img名", required = true, dataType = "Pet")
+    public Result systemPetpetImgSelect(@RequestBody Pet pet) {
         return petService.petImgSelect(pet);
     }
 
     @PostMapping("uploadImg")
+    @ApiOperation("上传图片")
+    @ApiImplicitParam(name = "file", value = "上传的文件,应该element的组件属性满足参数", required = true, dataType = "__File")
     public Result uploadByImage(@Param("file") MultipartFile file) throws IOException {
         Result result = new Result<>();
         //获取文件名
         String originalFilename = file.getOriginalFilename();
         String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
-        originalFilename= UUID.randomUUID()+substring;
+        originalFilename = UUID.randomUUID() + substring;
         //路径名
-        File file1 = new File(imgPath+originalFilename);
+        File file1 = new File(imgPath + originalFilename);
         file.transferTo(file1);
         ImgVo imgVo = new ImgVo();
         imgVo.setImgName(originalFilename);
-        imgVo.setImg(Base64Util.imgs(imgPath,originalFilename));
+        imgVo.setImg(Base64Util.imgs(imgPath, originalFilename));
         result.setCode("200");
         result.setData(imgVo);
         return result;
--- PetHomePlatform/src/main/java/com/example/platform/controller/UserController.java
+++ PetHomePlatform/src/main/java/com/example/platform/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.platform.controller;
+
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.CircleCaptcha;
 import com.example.platform.config.PageInfo;
 import com.example.platform.entity.User;
 import com.example.platform.service.UserService;
 import com.example.platform.uilt.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +18,14 @@ import java.io.IOException;
 @Log4j
 @RestController
 @RequestMapping("/user/")
+@Api(tags = "用户管理接口", description = "提供用户管理相关的 API")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("userCode")
+    @ApiOperation("获取验证码")
     public Result getCode() throws IOException {
         CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 5);
         String code = captcha.getCode();
@@ -27,26 +33,36 @@ public class UserController {
     }
 
     @PostMapping("userLogin")
+    @ApiOperation("用户登录")
+    @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")
     public Result systemUserLogin(@RequestBody User user){
         return userService.userLogin(user);
     }
 
     @PostMapping("userInsert")
+    @ApiOperation("插入用户")
+    @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")
     public Result systemUserInsert(@RequestBody User user){
         return userService.userInsert(user);
     }
 
     @DeleteMapping("userRemove")
+    @ApiOperation("删除用户")
+    @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")
     public Result systemUserRemove(@RequestBody User user){
         return userService.userRemove(user);
     }
 
     @PostMapping("userUpdate")
+    @ApiOperation("更新用户信息")
+    @ApiImplicitParam(name = "user", value = "用户实体", required = true, dataType = "User")
     public Result systemUserUpdate(@RequestBody User user){
         return userService.userUpdate(user);
     }
 
     @PostMapping("userFindList")
+    @ApiOperation("分页查询用户列表")
+    @ApiImplicitParam(name = "page", value = "分页信息,可进行msg=username筛选", required = true, dataType = "PageInfo")
     public Result systemUserList(@RequestBody PageInfo page){
         return userService.userFindByList(page);
     }
